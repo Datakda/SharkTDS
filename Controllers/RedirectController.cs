87e841d4ff62fb05e2190897ea4c97de40a49640
Datakda <gagan9393@gmail.com>
2021-07-21 22:18:26 +0300

    dev

--- Controllers/RedirectController.cs
+++ Controllers/RedirectController.cs
@@ -8,15 +8,20 @@ namespace SharkTDS.Controllers
 {
     public class RedirectController : Controller
     {  //https://localhost:44377/redirect?id=ssdsf&key=KEYWORDS
+        //Request.Headers["User-Agent"].ToString()
         public IActionResult Index(string id,string key)
         {
             if (id == null) 
             {
                 return StatusCode(404);
-
+                
             }
 
-            return Content("id");
+
+
+
+
+            return Content("ID="+id+" KEY="+key);
 
 
         }
